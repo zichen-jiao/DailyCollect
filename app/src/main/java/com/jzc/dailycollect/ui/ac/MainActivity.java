35cda866a1a85362edfa35f594da4d309e3c04a0
焦自辰 <Jzc@ChenXiS-MacBook-Pro.local>
2016-11-08 18:03:33 +0800

    初始化导入。 本项目主要收集日常工作中的开发时用到的知识点。归集整理。方便日后开发。

---
+++ app/src/main/java/com/jzc/dailycollect/ui/ac/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jzc.dailycollect.ui.ac;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jzc.dailycollect.R;
+
+/**
+ * 程序主页面
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
